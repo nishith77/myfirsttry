41bfb2e842e0ff8d62ac79a57b2380e20790f700
Nishith Mamtora <n_mamtora@yahoo.com>
2015-05-10 14:18:00 -0400

    This is my first commmit

---
+++ app.js
@@ -0,0 +1,12 @@
+var first_name = "Nishith";
+var last_name = "Mamtora";
+var age = 24;
+var Address = "24219 83rd ave";
+var fav_food = ["daal", "rice", "samusa", 1, 2, 3, 4];
+
+var complete_bio = [first_name, last_name, age, Address]
+
+console.log(last_name);
+
+alert(fav_food[1]);
+
